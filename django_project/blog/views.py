b2c3a4403839cb2b9704d0fcdb84eefe1b26d125
Odin <sergius.la@gmail.com>
2020-02-13 06:52:01 -0800

    models

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
-    context = {}
+    context = {
+        "postes": Post.objects.all()
+    }
     return render(request, "blog/home.html", context)
 
 def about(request):
